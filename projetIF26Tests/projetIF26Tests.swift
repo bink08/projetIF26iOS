728ba76502119bee407b8385e2cfb892828051ab
clément nivelet <clement.nivelet@clements-iMac.local>
2017-12-12 06:33:28 -0800

    Initial Commit

---
+++ projetIF26Tests/projetIF26Tests.swift
@@ -0,0 +1,36 @@
+//
+//  projetIF26Tests.swift
+//  projetIF26Tests
+//
+//  Created by clément nivelet on 12/12/2017.
+//  Copyright © 2017 if26. All rights reserved.
+//
+
+import XCTest
+@testable import projetIF26
+
+class projetIF26Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
